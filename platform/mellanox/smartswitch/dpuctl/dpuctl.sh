8469d6fe264db5ce5e6e4f97a33ee8ffd313ba9e
Gagan Punathil Ellath <gpunathilell@nvidia.com>
2025-07-31 15:07:24 -0700

    [202411][mellanox] Use the PF interface for the midplane communication with the DPU and rshim updates (#23533)
    [202411][mellanox] Use the PF interface for the midplane communication with the DPU and rshim updates

--- platform/mellanox/smartswitch/dpuctl/dpuctl.sh
+++ platform/mellanox/smartswitch/dpuctl/dpuctl.sh
@@ -20,10 +20,37 @@
 DARK_MODE=true
 
 bf3_pci_id="15b3:c2d5"
-dpu2pcie[0]="08:00.0"
-dpu2pcie[1]="07:00.0"
-dpu2pcie[2]="01:00.0"
-dpu2pcie[3]="02:00.0"
+
+# Function to get PCI addresses dynamically from platform.json
+get_dpu_pci_addresses() {
+    local dpu_list
+    local -A pci_to_dpu_map
+    
+    # Get list of all DPUs from platform.json
+    dpu_list=$(dpumap.sh listdpus 2>/dev/null)
+    if [[ $? -ne 0 ]]; then
+        echo "Error: Failed to get DPU list from platform.json" >&2
+        return 1
+    fi
+    
+    # Get PCI address for each DPU and build the mapping
+    while read -r dpu; do
+        if [[ -n "$dpu" ]]; then
+            rshim_bus_pci_addr=$(dpumap.sh dpu2rshim_bus_info "$dpu" 2>/dev/null)
+            if [[ $? -eq 0 && "$rshim_bus_pci_addr" != "null" ]]; then
+                pci_to_dpu_map["$rshim_bus_pci_addr"]="$dpu"
+            fi
+            # If DPU has older image, the bus_info pcie value should be considered 
+            pci_addr=$(dpumap.sh dpu2pcie "$dpu" 2>/dev/null)
+            if [[ $? -eq 0 && "$pci_addr" != "null" ]]; then
+                pci_to_dpu_map["$pci_addr"]="$dpu"
+            fi
+        fi
+    done <<< "$dpu_list"
+    
+    # Return the mapping as a string that can be eval'd
+    declare -p pci_to_dpu_map
+}
 
 if [[ -f /etc/mlnx/dpu.conf ]]; then
     . /etc/mlnx/dpu.conf
@@ -33,18 +60,28 @@ do_start() {
     if [[ $DARK_MODE == "true" ]]; then
         # By default all the DPUs are on. Power off the DPUs when is dark mode is required.
 
-        for dpu_id in ${!dpu2pcie[@]}; do
-            pci_id=$(lspci -n | grep "${dpu2pcie[$dpu_id]}" | awk '{print $3}')
-            if [[ $pci_id == $bf3_pci_id ]]; then
-                dpuctl dpu-power-off dpu${dpu_id} &
+        # Get PCI addresses dynamically
+        eval $(get_dpu_pci_addresses)
+        if [[ ${#pci_to_dpu_map[@]} -eq 0 ]]; then
+            echo "Error: No PCI addresses found from platform.json" >&2
+            exit 1
+        fi
+        
+        # Get all PCI addresses that match bf3_pci_id and extract their PCI IDs
+        pci_ids=$(lspci -Dn | grep "$bf3_pci_id" | awk '{print $1}')
+        
+        # For each PCI ID, find the corresponding DPU from the mapping
+        while read -r pci_id; do
+            if [[ -n "$pci_id" ]]; then
+                dpu_name=${pci_to_dpu_map[$pci_id]}
+                if [[ -n "$dpu_name" ]]; then
+                    dpuctl dpu-power-off "$dpu_name" &
+                fi
             fi
-        done
+        done <<< "$pci_ids"
 
         # Wait for all dpuctl processes to finish
         wait
-    else
-        # Start RSHIM per each DPU to create interfaces
-        systemctl start rshim-manager.service
     fi
 }
 
